93284a2a6c5719c37fa90b9ea16b57617dec760c
Steve Diabo <sdiabo@infusion.com>
2017-09-11 23:29:42 -0400

    - indenting issue on external.js - added loading spinner!

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,124 +4,130 @@
 
   $(function () {
 
+    // loading spinner overlay
+
+    $(window).on('load', function(){
+      $('footer').fadeOut(500);
+    });
+
   	// disable overscroll in non-scrolling areas
 
-	$('header, main .view h3').on('touchmove',function(event) {
-		event.preventDefault();
-	});
+  	$('header, main .view h3').on('touchmove',function(event) {
+  		event.preventDefault();
+  	});
 
-	//owl carousel init
+  	//owl carousel init
 
-	$('.owl-carousel').owlCarousel({
-	    loop: false,
-	    responsiveClass:true,
-	    responsive:{
-	        0:{
-	            items: 1,
-	            nav: true
-	        },
-	        480:{
-	            items: 2,
-	            nav: true
-	        },
-	        768:{
-	            items: 2,
-	            stagePadding: 43,
-	            margin: 24,
-	            nav: true
-	        },
-	        1024:{
-	            items: 3,
-	            stagePadding: 43,
-	            margin: 20,
-	            nav: true
-	        },
-	        1440:{
-	            items: 4,
-	            stagePadding: 43,
-	            margin: 20,
-	            nav: true
-	        }
-	    }
-	});
+  	$('.owl-carousel').owlCarousel({
+  	    loop: false,
+  	    responsiveClass:true,
+  	    responsive:{
+  	        0:{
+  	            items: 1,
+  	            nav: true
+  	        },
+  	        480:{
+  	            items: 2,
+  	            nav: true
+  	        },
+  	        768:{
+  	            items: 2,
+  	            stagePadding: 43,
+  	            margin: 24,
+  	            nav: true
+  	        },
+  	        1024:{
+  	            items: 3,
+  	            stagePadding: 43,
+  	            margin: 20,
+  	            nav: true
+  	        },
+  	        1440:{
+  	            items: 4,
+  	            stagePadding: 43,
+  	            margin: 20,
+  	            nav: true
+  	        }
+  	    }
+  	});
 
-	// jquery css scrollbar init
+  	// jquery css scrollbar init
 
-	$('.scrollbar-inner').scrollbar();
+  	$('.scrollbar-inner').scrollbar();
 
-  // listeners
+    // listeners
 
-  $('.touchable').on('mousedown touchstart', function() {
-    $(this).addClass('pressed');
-    $(this).on('mouseup touchend', function() {
-      $(this).removeClass('pressed');
+    $('.touchable').on('mousedown touchstart', function() {
+      $(this).addClass('pressed');
+      $(this).on('mouseup touchend', function() {
+        $(this).removeClass('pressed');
+      });
     });
-  });
 
-	// nav buttons
+  	// nav buttons
 
-  function openView(i) {
+    function openView(i) {
 
-    var href = $(i).attr('data-href');
+      var href = $(i).attr('data-href');
 
-    $('body').addClass('nav-tapped');
+      $('body').addClass('nav-tapped');
 
-    $('*[data-view-name=' + href + ']').addClass('shown');
+      $('*[data-view-name=' + href + ']').addClass('shown');
 
-    // refactor the shit below to use timed css keyframe anims
+      // refactor the shit below to use timed css keyframe anims
 
-    setTimeout(function() {
-      $('body').addClass('header-locked');
-      $('*[data-view-name=' + href + ']').addClass('locked');
-    },500);
+      setTimeout(function() {
+        $('body').addClass('header-locked');
+        $('*[data-view-name=' + href + ']').addClass('locked');
+      },500);
 
-    setTimeout(function() {
-      $('*[data-view-name=' + href + ']').addClass('back-btn-deployed');
-    },1000);
+      setTimeout(function() {
+        $('*[data-view-name=' + href + ']').addClass('back-btn-deployed');
+      },1000);
 
-    setTimeout(function() {
-      $('body.first-use').addClass('first-use-animations');
-    },1500);
+      setTimeout(function() {
+        $('body.first-use').addClass('first-use-animations');
+      },1500);
 
-    setTimeout(function() {
-      $('body.first-use').removeClass('first-use first-use-animations');
-    },3500);
+      setTimeout(function() {
+        $('body.first-use').removeClass('first-use first-use-animations');
+      },3500);
 
-  }
+    }
 
-  // detect touch or assume mouse based on first interaction
+    // detect touch or assume mouse based on first interaction
 
-  window.addEventListener('touchstart', function onFirstTouch() {
-    document.body.classList.add('using-touch');
-    window.removeEventListener('touchstart', onFirstTouch, false);
-  }, false);
+    window.addEventListener('touchstart', function onFirstTouch() {
+      document.body.classList.add('using-touch');
+      window.removeEventListener('touchstart', onFirstTouch, false);
+    }, false);
 
-  $('nav ul li.nav-link').on('touchend mouseup', function() {
-    openView(this);
-  });
+    $('nav ul li.nav-link').on('touchend mouseup', function() {
+      openView(this);
+    });
 
-	// back buttons
+  	// back buttons
 
-	$('main .view h3 .btn-back').on('mouseup touchend', function() {
+  	$('main .view h3 .btn-back').on('mouseup touchend', function() {
 
-		var href = $(this).parents().parents().attr('data-view-name');
+  		var href = $(this).parents().parents().attr('data-view-name');
 
-		$('*[data-view-name=' + href + ']').removeClass('back-btn-deployed');
+  		$('*[data-view-name=' + href + ']').removeClass('back-btn-deployed');
 
-		setTimeout(function() {
-			$('*[data-view-name=' + href + ']').removeClass('locked');
-			$('body').removeClass('header-locked');
-		},500);
+  		setTimeout(function() {
+  			$('*[data-view-name=' + href + ']').removeClass('locked');
+  			$('body').removeClass('header-locked');
+  		},500);
 
-		setTimeout(function() {
-			$('body').removeClass('nav-tapped');
-		},1000);
+  		setTimeout(function() {
+  			$('body').removeClass('nav-tapped');
+  		},1000);
 
-		setTimeout(function() {
-			$('*[data-view-name=' + href + ']').removeClass('shown');
-		},1500);
+  		setTimeout(function() {
+  			$('*[data-view-name=' + href + ']').removeClass('shown');
+  		},1500);
 
-	});
+  	});
 
 
   });
